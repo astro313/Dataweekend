4e911c89183f53791dec94933bf55492fa5e1802
Daisy Leung <leungtszkuk@gmail.com>
2018-11-04 17:15:57 -0500

    make it a func

---
+++ DeepLearning/create_model.py
@@ -0,0 +1,25 @@
+'''
+
+source activate py36
+
+'''
+
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import *
+
+
+def create_mod():
+    training_data_df = pd.read_csv("sales_data_training_scaled.csv")
+
+    X = training_data_df.drop('total_earnings', axis=1).values    # split off feature
+    Y = training_data_df[['total_earnings']].values
+
+    # Define the model
+    model = Sequential()
+    model.add(Dense(50, input_dim=9, activation='relu'))   # 50 nodes, first layer need input_dim = # of features; densely connected: each node connected to each other node in another layer
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(1, activation='linear'))         # finally, 1 node for prediction. Linear since it's a single linear value
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    return model, X, Y
