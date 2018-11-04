41d109625e6fa117977e514a51366c8b0441e3c3
Daisy Leung <leungtszkuk@gmail.com>
2018-11-04 18:11:50 -0500

    name layers

--- DeepLearning/create_model.py
+++ DeepLearning/create_model.py
@@ -17,9 +17,9 @@ def create_mod():
 
     # Define the model
     model = Sequential()
-    model.add(Dense(50, input_dim=9, activation='relu'))   # 50 nodes, first layer need input_dim = # of features; densely connected: each node connected to each other node in another layer
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(1, activation='linear'))         # finally, 1 node for prediction. Linear since it's a single linear value
+    model.add(Dense(50, input_dim=9, activation='relu', name='layer_1'))   # 50 nodes, first layer need input_dim = # of features; densely connected: each node connected to each other node in another layer
+    model.add(Dense(100, activation='relu', name='layer_2'))
+    model.add(Dense(50, activation='relu', name='layer_3'))
+    model.add(Dense(1, activation='linear', name='output_layer'))         # finally, 1 node for prediction. Linear since it's a single linear value
     model.compile(loss="mean_squared_error", optimizer="adam")
     return model, X, Y
