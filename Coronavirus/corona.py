a0c3adc298d977869773523946211fbc3d356abc
Daisy Leung <leungtszkuk@gmail.com>
2020-03-11 17:38:04 -0400

    rewrite func to parse each element from table than split off the strings per row to deal with missing entries in table

--- Coronavirus/corona.py
+++ Coronavirus/corona.py
@@ -15,71 +15,60 @@ class Coronavirus():
 
     def get_table(self):
         self.table = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody')
-
+        self.colNames = self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/thead').text.split(' ')
+        self.colNames[-3] += self.colNames[-2] + self.colNames[-1]
+        self.colNames.remove(self.colNames[-2])
+        self.colNames.remove(self.colNames[-1])
+        self.Ncol = len(self.colNames)
 
     def get_table_all_country(self):
-        tab = self.table.text
-        self.tab_splited_by_country = tab.split('\n')
-        # for each row, split by calling get_number_per_row_from_table(), put into dataframe
-        df = pd.Series()
-        for rr in self.tab_splited_by_country:
-            s = self.get_number_per_row_from_table(rr)
-            df = pd.concat([df, s], axis=1)
-            # sdf[s[0]]
-        df = df.T.loc[1:]
-        df = df.reset_index()
-        df = df.drop(['index'], axis=1)
-        assert len(df.columns) == 8
-        df = df.set_index('country')
+        # instead of reading the whole row, we will read from entry by entry
+        tab = self.table.text.split('\n')
+        nCountries = len(tab)
+
+        df = None
+        for i in range(1, nCountries):
+            rowNum = str(i)
+            row = self.table.find_elements_by_xpath('//tr[' + rowNum + ']')
+            if type(row) is list and len(row) > 1:
+                for kk in range(len(row)):
+                    if row[kk].text.split('\n') == self.driver.find_element_by_xpath('//*[@id="main_table_countries"]/thead').text.split('\n'):
+                        # same as colhead
+                        continue
+                    df = self.parse_row_to_dict(row[kk], df)
+            elif type(row) is list and len(row) == 1:
+                row = row[0]
+                df = self.parse_row_to_dict(row, df)
+            else:
+                df = self.parse_row_to_dict(row, df)
+
+        df = self.dict_to_df(df)
         return df
 
-    def __combine_splited_country_name(self):
-        try:
-            float(self.data[1])
-        except ValueError:
-            self.data[0] += self.data[1]
-            self.data.remove(self.data[1])
-            self.__combine_splited_country_name(1)
+    def parse_row_to_dict(self, row, df):
+        if df is None:
+            df = {}
 
-    def get_number_per_row_from_table(self, row):
-        import types
-        if type(row) is types.UnicodeType:
-            data = row.split(" ")
-        else:
-            # row is selenium.webdriver.remote.webelement.WebElement
-            data = row.text.split(" ")
+        entry = []
+        for j in range(1, self.Ncol+1):
+            entry.append(row.find_element_by_xpath("td[" + str(j) + "]").text)
+        entry[1:] = [i.replace(',', '') for i in entry[1:]]
+        entry[1:] = [i.replace('+', '') for i in entry[1:]]
 
-        # clean up
-        # replace S. with South
-        # replace N. with North
-        data = [i.replace('S.', 'South') for i in data]
-        data = [i.replace('N.', 'North') for i in data]
-        data = [i.replace(',', '') for i in data]
-        self.data = [i.replace('+', '') for i in data]
+        if "" in entry:
+            # replace with -999
+            for idx, lll in enumerate(entry):
+                if lll == '':
+                    entry[idx] = '-999'
+        entry[1:] = [float(i) for i in entry[1:]]
+        df[entry[0]] = entry[1:]
+        return df
 
-        # if country name is split into 2 elements in the list
-        # combine them
-        # hopefully no country names split into 3 or more elements
-        self.__combine_splited_country_name())
-
-        # convert numerical data into float
-        try:
-            self.data[1:] = [float(i) for i in self.data[1:]]
-        except:
-            import pdb; pdb.set_trace()
-
-        # some countries have msising col
-        if len(self.data) < 9:
-            # replace all entries w/ nan
-            country = self.data[0]
-            self.data = list(np.zeros(9))
-            self.data[0] = country
-            self.data[1:] = [np.nan for i in self.data[1:]]
-
-        # put in Pd.Series
-        assert len(self.data[:-1]) == 8
-        s = pd.Series(self.data[:-1], index=['country', 'total_cases', 'new_cases', 'total_deaths', 'new_deaths', 'active_cases', 'total_recovered', 'serious_critical'])
-        return s
+    def dict_to_df(self, df):
+        df = pd.DataFrame(df).T
+        self.colNames[1:] = [i.replace('\n', ' ') for i in self.colNames[1:]]
+        df.columns=self.colNames[1:]
+        return df
 
 
     def get_country_from_table(self, country):
@@ -88,13 +77,17 @@ class Coronavirus():
         """
         country_element = self.table.find_element_by_xpath("//td[contains(text(), " + country + ")]")   # country
         row = country_element.find_element_by_xpath("./..")
-        s = self.get_number_per_row_from_table(row)
-        return s
+        df = self.parse_row_to_dict(row, df=None)
+        df = self.dict_to_df(df)
+        return df
 
 
 if __name__ == '__main__':
     bot = Coronavirus()
-    series = bot.get_country_from_table('China')
-    df = bot.get_table_all_country()
+    df1 = bot.get_country_from_table('China')
+    df2 = bot.get_table_all_country()
+
+    print(df1)
+    print(df2)
 
 
